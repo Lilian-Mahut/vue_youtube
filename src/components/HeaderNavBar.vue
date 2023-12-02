4cef8450818f423db188038c7c8a6c747a124e39
Lilian Mahut <lilian.mahut@pm.me>
2023-12-02 12:13:40 +0100

    update routing & set YT page

---
+++ src/components/HeaderNavBar.vue
@@ -0,0 +1,14 @@
+<script setup lang="ts">
+import NavBarItem from './NavBarItem.vue'
+import YoutubeIcon from './icons/IconYoutube.vue'
+</script>
+
+<template>
+  <NavBarItem>
+    <template #icon>
+      <a href="" target="_blank" rel="">
+        <YoutubeIcon />
+      </a>
+    </template>
+  </NavBarItem>
+</template>
---
+++ src/views/YoutubeView.vue
@@ -0,0 +1,15 @@
+<script setup lang="ts">
+import HeaderNavBar from '../components/HeaderNavBar.vue'
+// import VideoPlaying from '../components/VideoPlaying.vue'
+// import AnotherVideos from '../components/AnotherVideos.vue'
+</script>
+
+<template>
+  <header>
+    <HeaderNavBar />
+  </header>
+  <main>
+    <section></section>
+    <aside></aside>
+  </main>
+</template>
